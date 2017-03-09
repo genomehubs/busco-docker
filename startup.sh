63e7bc88a2f91171b637ce9cde8dcfdbac485cb3
rjchallis <rjchallis@gmail.com>
2017-03-09 12:47:56 +0000

    modify output filenames

--- startup.sh
+++ startup.sh
@@ -12,9 +12,9 @@ BUSCO.py \
        -o output \
        $@
 
-mv run_output/short_summary_output.txt /out/$INFILE.short_summary.txt
-gzip -c run_output/full_table_output.tsv > /out/$INFILE.full_table.tsv.gz
-mv run_output/single_copy_busco_sequences ${INFILE}_single_copy_busco_sequences
-tar -zcf ${INFILE}_single_copy_busco_sequences.tar.gz ${INFILE}_single_copy_busco_sequences
-mv ${INFILE}_single_copy_busco_sequences.tar.gz /out
+mv run_output/short_summary_output.txt /out/$INFILE.busco.short_summary.txt
+gzip -c run_output/full_table_output.tsv > /out/$INFILE.busco.full_table.tsv.gz
+mv run_output/single_copy_busco_sequences ${INFILE}.busco.single_copy
+tar -zcf ${INFILE}.busco.single_copy.tar.gz ${INFILE}.busco.single_copy
+mv ${INFILE}.busco.single_copy.tar.gz /out
 
